88f2cc4363534ac8da70e69ff4090b9ba504c0b2
david.petticrew <david.petticrew@CRO-24-AV>
2018-09-11 09:16:44 +0100

    added textbox

--- hello/Form1.cs
+++ hello/Form1.cs
@@ -19,7 +19,7 @@ namespace hello
 
         private void button1_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("Hello");
+            MessageBox.Show("Hello " + txtBxName.Text);
         }
     }
 }
